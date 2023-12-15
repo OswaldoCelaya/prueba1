6fa7b6e561da1aa37881c6e3ce2abe1596c0b85e
OswaldoCelaya <153897743+OswaldoCelaya@users.noreply.github.com>
2023-12-15 01:55:29 -0600

    Add files via upload

---
+++ prueba.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class prueba{
+	public static void main (String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		System.out.println ("Ingresa tú nombre");
+		String nombre=in.next();
+		System.out.println ("Binevenido "+nombre);
+}
+}
